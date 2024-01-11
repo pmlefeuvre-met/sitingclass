98761e20687e14b0edc329ee41c84c56cee3f499
pmlefeuvre_met <pierreml@met.no>
2024-01-11 15:11:17 +0100

    Add test for horizon

---
+++ tests/testthat/test-horizon.R
@@ -0,0 +1,32 @@
+testthat::test_that("compute horizon works", {
+
+  # Load the station metadata
+  stn.id      <- 18700
+  stn.centre  <- cbind(260966.8, 6652718)
+  colnames(stn.centre) <- c("X", "Y")
+
+  # Set expected horizon
+  horizon.expected <- data.frame(azimuth=c(0, 350, 340, 330, 320, 310, 300,
+                                           290, 280, 270, 260, 250, 240, 230,
+                                           220, 210, 200, 190, 180, 170, 160,
+                                           150, 140, 130, 120, 110, 100, 90,
+                                           80, 70, 60, 50, 40, 30, 20, 10),
+                                 horizon_height=c(9.9, 2.3, 3.1, 20.9, 12.2,
+                                                  32.8, 6.6, 9.1, 10.8, 9.8, 8,
+                                                  6.6, 12.6, 14.4, 11.8, 22.7,
+                                                  63.3, 63.3, 63.3, 63.3, 63.3,
+                                                  63.3, 7.3, 7.9, 12.5, 9.7,
+                                                  10.6, 9.1, 7.8, 21.4, 22.8,
+                                                  18.3, 18.6, 18.4, 17.7, 9.9)
+  )
+
+  # Load a digital elevation model
+  dsm   <- download_dem_kartverket(stn.id, stn.centre, name="dom",
+                                   dx=100, resx=1,path = )
+
+  # Compute the horizon
+  horizon.computed <- compute_horizon(stn.centre, dsm)
+
+  # Compare result and expected horizon
+  testthat::expect_equal(round(horizon.computed,1), horizon.expected)
+})
