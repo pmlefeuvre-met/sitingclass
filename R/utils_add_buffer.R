143c856e7d22c59d29f6a8c4d821126235dc78b9
Pierre-Marie Lefeuvre <pierreml@met.no>
2024-08-19 14:02:12 +0200

    line for buffer

--- R/utils_add_buffer.R
+++ R/utils_add_buffer.R
@@ -53,11 +53,11 @@ add_buffer <- function(g = NULL,
   # Add buffers
   bbuf1 <- terra::buffer(v, buf1)
   g <- g + geom_sf(data = bbuf1, fill = NA, color = "black", linewidth=2)
-  g <- g + geom_sf(data = bbuf1, fill = NA, color = "white", linetype = c(800, 600), linewidth=2)
+  g <- g + geom_sf(data = bbuf1, fill = NA, color = "white", linetype = "longdash", size=10)
   if (!is.null(buf2)) {
     bbuf2 <- terra::buffer(v, buf2)
     g <- g + geom_sf(data = bbuf2, fill = NA, color = "black", linewidth=2)
-    g <- g + geom_sf(data = bbuf2, fill = NA, color = "white", linetype = c(800, 600), linewidth=2)
+    g <- g + geom_sf(data = bbuf2, fill = NA, color = "white", linetype = "longdash", size=10)
   }
 
   # Add buffer legend as arrow segments
