e93fbc17552471ed35a4103a134b41b117de8189
Pierre-Marie Lefeuvre <pierreml@met.no>
2025-01-05 11:41:01 +0100

    Add pipeline function for computing class

---
+++ R/compute_landtype_horizon_class.R
@@ -0,0 +1,47 @@
+#' Pipeline to compute siting class
+#'
+#' The pipeline derives the exposure of a station based WMO/met.no siting
+#' classification and includes four input functions:
+#'\code{"download_dem_kartverket"}, \code{"compute_landtype"},
+#' \code{"compute_landtype_distance"} and \code{"compute_horizon_max"}.
+#' They provide the necessary input for \code{"compute_class"}
+#'
+#' @param stn A SpatVector with station attribute `stationid` from
+#'        \code{"get_latlon_frost"}
+#'
+#' @return None
+#'
+#' @examples
+#' # Pipeline to compute necessary input for deriving temperature siting class
+#' # compute_landtype_horizon_class(stn)
+#'
+#' @export
+
+compute_landtype_horizon_class <- function(stn) {
+
+  # Load a digital elevation model
+  dem <- download_dem_kartverket(stn, name = "dtm",f_overwrite = TRUE)
+
+    # Compute land type
+  landtype <- compute_landtype(stn,
+                               f_plot = TRUE)
+
+  # Compute land type distance to station
+  landtype_dist <- compute_landtype_distance(stn,
+                                             landtype,
+                                             f_plot = TRUE)
+  # Compute maximum horizon
+  horizon_max <- compute_horizon_max(stn,
+                                     step = 0.1,
+                                     f_plot_polygon = FALSE)
+
+  # Compute class
+  class <- compute_class(stn,
+                         landtype_dist,
+                         horizon_max,
+                         dem,
+                         test_type = "WMO",
+                         f_plot = TRUE)
+
+  return(class)
+}
