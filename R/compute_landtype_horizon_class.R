b76e65a6dc2391354e68b7faf23aa5c18edc7d12
Pierre-Marie Lefeuvre <pierreml@met.no>
2025-04-11 16:06:35 +0200

    Deploy pkgdown build for github pages

--- R/compute_landtype_horizon_class.R
+++ R/compute_landtype_horizon_class.R
@@ -13,6 +13,9 @@
 #' @return None
 #'
 #' @examples
+#' # Get station metadata
+#' stn <- get_metadata_frost(stationid = 73466, dx = 100, resx = 1)
+#'
 #' # Pipeline to compute input for deriving air_temperature siting class
 #' # compute_landtype_horizon_class(stn)
 #'
@@ -33,8 +36,9 @@ compute_landtype_horizon_class <- function(stn,
                                              landtype,
                                              f_plot = f_plot)
   # Compute maximum horizon
+  step=.01
   horizon_max <- compute_horizon_max(stn,
-                                     step = 0.01,
+                                     step = step,
                                      f_output_all = TRUE)
 
   # Keep only obstacles above mountains
@@ -42,6 +46,33 @@ compute_landtype_horizon_class <- function(stn,
   horizon_obstacles["horizon_dsm"] <- horizon_obstacles["horizon_dsm"] *
     (horizon_max$horizon_dsm > horizon_max$horizon_demkm)
   names(horizon_obstacles) <-  c("azimuth", "horizon_height", "range")
+
+  horizon_obstacles[,"dhorizon"] <- c(0,diff(horizon_obstacles[,"horizon_height"])/step)
+  dh_threshold <-  2/step
+  n_pos <- which(horizon_obstacles["dhorizon"]>=dh_threshold)
+  n_neg <- which(horizon_obstacles["dhorizon"]<=-dh_threshold)
+  n_range <- (n_pos[length(n_pos)]-1/step):(n_neg[1]+1/step)
+  if(diff(range(n_range))*step < 25){
+    horizon_obstacles[,"horizon_height"] <- 0 #index of horizon is reversed
+  }
+  g0 <- ggplot(data = horizon_max, aes(x=azimuth, y=horizon_dsm)) +
+    geom_line() + theme_minimal()
+
+  g1 <- ggplot(data = horizon_obstacles, aes(x=azimuth, y=horizon_height)) +
+    geom_line() + theme_minimal()
+
+  g2 <- ggplot(data = horizon_obstacles, aes(x=azimuth, y=dhorizon))+
+    geom_line() + theme_minimal() + geom_hline(yintercept = c(dh_threshold,-dh_threshold),colour="grey") +
+    geom_point(data=horizon_obstacles[n_pos,], aes(x=azimuth,y=dhorizon),colour="coral") +
+    geom_point(data=horizon_obstacles[n_neg,], aes(x=azimuth,y=dhorizon),colour="skyblue")
+  g0/g1/g2
+
+  # unique(floor(horizon_max$azimuth[n_neg]))
+  # unique(ceiling(horizon_max$azimuth[n_pos]))
+  # c(sort(horizon_max$azimuth[n_neg])[1],sort(horizon_max$azimuth[n_pos])[length(n_neg)])
+  # c(n_neg[1],n_pos[length(n_neg)])
+
+  # DETERMINE PERCENTAGE ABOVE HORIZON TO MAKE ALGO KINDER 44300 SÆRHEIM
 
   # Compute class
   class <- compute_class_air_temperature(stn,
